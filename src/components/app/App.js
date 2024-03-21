0292bdc43f4724c81a7dc071a2ef64d97cbb3c05
josteen1luv <josteen1luv@gmail.com>
2024-03-21 21:21:38 +0100

    done project
    Signed-off-by: josteen1luv <josteen1luv@gmail.com>

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import AppHeader from "../appHeader/AppHeader";
+
+import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages'
+const App = () =>{
+    return (
+        <BrowserRouter>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path={"/"} element={<MainPage/>}/>
+                        <Route path = {"/comics"} element={<ComicsPage/>}/>
+                        <Route path = {"/comics/:comicId"} element={<SingleComicPage/>}/>
+                        <Route path={"*"} element={<Page404/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App;
